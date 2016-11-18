7085ceb637815d2ccc34f35a073fe1be7d8cb29d
grance1 <2397580855@qq.com>
2016-11-18 21:43:34 +0800

    this is class file

--- a/class1.py
+++ a/class1.py
@@ -26,3 +26,18 @@ class Student(object):
 bart = Student('Bart Simpson', 59)
 print bart.print_score()
 print bart.get_grade()
+
+
+bart = Student('Bart Simpson',98)
+print bart.score
+bart.score = 59
+print bart.score
+
+class Student(object):
+    def __init__(self,name,score):
+        self.__name = name
+        self.__score = score
+    def print_score(self):
+        print('%s: %s' % (self.__name, self.__score))
+bart = Student('Bart Simpson', 98)
+print  bart.__name
