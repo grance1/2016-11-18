38e5566f8eecad3eb29e138a2e7ae8b1a5d661fc
grance1 <2397580855@qq.com>
2016-11-18 21:33:37 +0800

    this is class file

---
+++ a/class1.py
@@ -0,0 +1,28 @@
+class Student(object):
+    def __init__(self,name,score):
+        self.name = name
+        self.score = score
+bart = Student('Bart Simpson',59)
+print bart.name
+print bart.score
+
+def print_score(std):
+    print('%s:%s'%(std.name,std.score))
+
+print_score(bart)
+class Student(object):
+    def __init__(self,name,score):
+        self.name = name
+        self.score = score
+    def print_score(self):
+        print('%s:%s' % (self.name, self.score))
+    def get_grade(self):
+        if self.score >= 90:
+            return 'A'
+        if self.score >= 60:
+            return 'B'
+        else:
+            return 'C'
+bart = Student('Bart Simpson', 59)
+print bart.print_score()
+print bart.get_grade()
